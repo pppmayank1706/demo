312af30b19d94a03bae0132a316e237aa9bebc88
pppmayank1706 <99557995+pppmayank1706@users.noreply.github.com>
2022-03-12 22:56:31 +0530

    Delete 05dbboot directory

--- 05dbboot/src/main/java/com/anubhavtrainings/service/IAddressPersistence.java
+++
@@ -1,9 +0,0 @@
-package com.anubhavtrainings.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.anubhavtrainings.entities.address;
-
-public interface IAddressPersistence extends JpaRepository<address, String> {
-
-}
